3e0cf910c50fd20bb4e5ab6dd7fd4e5f6878581f
shubhamranpura123@gmail.com <shubhamranpura123@gmail.com>
2024-12-23 14:31:11 +0530

    react Router

--- react-Router/src/Components/Github/Github.jsx
+++ react-Router/src/Components/Github/Github.jsx
@@ -11,7 +11,7 @@ function Github() {
     //  .then( data => {
     //     console.log(data);  
     //     setData(data)  
-    //  })
+    //  })  
     // } ,[]);
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
